e67dac12dd9bde6264038c775e2995577e2985ac
Salo <2680914103@qq.com>
2015-03-22 12:30:07 +0800

    integerot

--- Farseer_Project/SLFarseer_Mac/OCViewController.m
+++ Farseer_Project/SLFarseer_Mac/OCViewController.m
@@ -32,6 +32,13 @@ static void *AssociatedObjectHandle;
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
+    NSDictionary *responseDictionary = @{
+                                         @"NextUploadDate": @([NSDate timeIntervalSinceReferenceDate])
+                                         };
+    NSData *data = [NSJSONSerialization dataWithJSONObject:responseDictionary options:NSJSONWritingPrettyPrinted error:nil];
+    NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
 }
 
 - (void)setRepresentedObject:(id)representedObject {
